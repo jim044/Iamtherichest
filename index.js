cc580a1fea376c809db0fc20e74658e2d28f1928
root <root@debian>
2018-10-28 17:12:39 +0100

    First update

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+require('./lib/accueil.js')(app)
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
